0988f76d8a0d5ea37a9daf298ad07b85d39826d7
lexyerresta <errestlegends@gmail.com>
2023-12-21 09:28:54 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,27 @@
+import AnimeList from "@/components/AnimeList"
+import Header from "@/components/AnimeList/Header"
+import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/libs/api-libs"
+
+
+const Page = async () => {
+  const topAnime = await getAnimeResponse("top/anime", "limit=8")
+  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
+  recommendedAnime = reproduce(recommendedAnime, 4)
+
+  return (
+    <>
+      {/* Most Popular Anime */}
+      <section>
+        <Header title="Most Popular Anime" LinkTitle="See All" LinkHref="/popular" />
+        <AnimeList api={topAnime} />
+      </section>
+      {/* Anime Recommendations */}
+      <section>
+        <Header title="Anime Recommendations" LinkTitle="See All" LinkHref="/popular" />
+        <AnimeList api={recommendedAnime} />
+      </section>
+    </>
+  )
+}
+
+export default Page
